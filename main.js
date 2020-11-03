b5352e06833e52311fecb19a533e1a9629045519
Darlin Artiles <59721015+NerdyDiva@users.noreply.github.com>
2020-11-03 07:48:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.header').height($(window).height());
+   
+    $(".navbar a").click(function(){
+        $("body,html").animate({
+            scrollTop:$("#" + $(this).data('value')).offset().top
+        },1000)
+     
+    })
+
+    $("#links a").click(function(){
+        $("body,html").animate({
+            scrollTop:$("#" + $(this).data('value')).offset().top
+        },1000)
+     
+    })
+
+    $(".description a").click(function(){
+        $("body,html").animate({
+            scrollTop:$("#" + $(this).data('value')).offset().top
+        },1000)
+     
+    })
+   
+   })
